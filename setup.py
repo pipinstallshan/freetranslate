07c2cfaa67c26f670766ede0101cc8e00077b176
pipinstallshan <shan5889@outlook.com>
2024-11-18 15:36:17 +0500

    Initial commit: Translate No Limit library

--- setup.py
+++ setup.py
@@ -16,10 +16,26 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/pipinstallshan/freetranslate",
+    project_urls={
+        "Bug Tracker": "https://github.com/pipinstallshan/freetranslate/issues",
+        "Documentation": "https://github.com/pipinstallshan/freetranslate#readme",
+        "Source Code": "https://github.com/pipinstallshan/freetranslate",
+    },
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7"
+    python_requires=">=3.7",
+    include_package_data=True,
+    keywords="translation translate free python-library",
+    license="MIT",
 )
